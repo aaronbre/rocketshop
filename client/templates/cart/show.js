e6d1cd40beb0165ef0d879a94821189c8ba541a0
aaronbre <aribrecherdev@gmail.com>
2016-11-29 11:44:22 -0500

    first commit working on shopping cart

---
+++ client/templates/cart/show.js
@@ -0,0 +1,52 @@
+/**
+ * Created by aaronbrecher on 11/28/16.
+ */
+Template.cartShow.helpers({
+  
+   cart: function () {
+     currentCart = Carts.getCart(userKey);
+     return currentCart;
+   },
+  
+   thereAreNo: function (items) {
+     return items === 0;
+   } 
+});
+
+Template.cartShow.events({
+  'click .remove-from-cart': function (e) {
+    e.preventDefault();
+    removeFromCart(this.sku, function (err, res) {
+      if (err)
+        console.log(err);
+      else{
+        if (currentCart.items.length === 0)
+          Router.go('homeIndex');
+      }
+    });
+  },
+
+  'change .item-qty': function (e) {
+    var rawValue = e.target.value;
+    if(isNaN(rawValue))
+      alert('you did not enter a number');
+    else{
+      var number = parseInt(rawValue);
+      var name = this.name;
+      if (number <= 0){
+        removeFromCart(this.sku);
+      }
+      else{
+        this.quantity = number;
+        updateCart(this.sku, this.quantity, function (err, res) {
+          if(err)
+            alert('was not able to update');
+          else
+            alert('updated ' + name);
+        });
+      }
+    }
+  },
+
+
+});
